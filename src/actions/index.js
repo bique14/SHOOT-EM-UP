32fd516091858f1b74c16ae873b08da53d3b2871
bique14 <peerasorn14@gmail.com>
2018-08-20 21:23:15 +0700

    add action and reducer for login and update leaderboard

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
+export const LEADERBOARD_LOADED = 'LEADERBOARD_LOADED'
+export const LOGGED_IN = 'LOGGED_IN'
 export const MOVE_OBJECTS = 'MOVE_OBJECTS'
 export const START_GAME = 'START_GAME'
+
+export const leaderboardLoaded = (players) => ({
+  // When the real-time service sends the list of players, 
+  // you will use this action to update the Redux store with these players.
+  type: LEADERBOARD_LOADED,
+  players,
+})
+
+export const loggedIn = (players) => ({
+  type: LOGGED_IN,
+  players,
+})
 
 export const moveObjects = (mousePosition) => ({
   type: MOVE_OBJECTS,
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,4 +1,6 @@
 import {
+  LEADERBOARD_LOADED, 
+  LOGGED_IN,
   MOVE_OBJECTS, 
   START_GAME
 } from '../actions'
@@ -11,6 +13,8 @@ const initialGameState = {
   lives: 3,
   flyingObjects: [],
   lastObjectCreatedAt: new Date(),
+  currentPlayer: null,
+  playes: null,
 }
 
 const initialState = {
@@ -20,6 +24,16 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch(action.type) {
+    case LEADERBOARD_LOADED:
+      return {
+        ...state,
+        players: action.players,
+      }
+    case LOGGED_IN:
+      return {
+        ...state,
+        currentPlayer: action.players,
+      }
     case MOVE_OBJECTS:
       return moveObjects(state, action)
     case START_GAME:
